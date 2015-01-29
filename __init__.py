8cafca6defe9700c654d5fb426ced0aa7f0fbb5f
mhnbcu <mhnbcu@users.noreply.github.com>
2015-01-29 10:33:54 -0800

    manually run register_signal_handlers() in 1.6.x

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 default_app_config = 'wagtailbakery.apps.WagtailBakeryAppConfig'
 
+from django import get_version
+if '1.7' > get_version():
+    from . import signal_handlers
+    signal_handlers.register_signal_handlers()
